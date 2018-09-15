0a338bdf12b2bc0bb824661846f5d5b74cf6b37b
Dmytro <dmytro@proisp.no>
2018-09-15 12:58:03 +0300

    Make automatic installation of new VM and run VNC server

--- InfrastructureHandler.py
+++ InfrastructureHandler.py
@@ -11,13 +11,32 @@ class InfrastructureHandler:
 
         installed, noninstalled = vb_instalation_checking.communicate()
         if noninstalled:
-            instalation_proc = subprocess.Popen(
-                ['apt-get', 'install', 'virtualbox', '-y'],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE)
-            out, er = instalation_proc.communicate()
-            instalation_proc
-            print(out)
+            subprocess.call(['sudo -S apt-get', 'install', 'virtualbox', '-y'])
         else:
             print('VMBOX is installed')
+
+    def install_vm(self, os, ram, hdd):
+        if os == 'ubuntu':
+
+            subprocess.call(['vboxmanage', 'createvm', '--name', 'UbuntuTest', '--ostype', 'Ubuntu_64', '--register'])
+            subprocess.call(['vboxmanage', 'modifyvm', 'UbuntuTest', '--cpus', '1', '--memory', str(ram), '--nic1', 'nat',
+                             '--boot1', 'dvd'])
+            subprocess.call(['vboxmanage', 'createmedium', 'disk',
+                             '--filename', '/home/chevil/VirtualBox VMs/UbuntuTest/ubuntu1604.vdi', '--size', str(hdd)])
+            subprocess.call(['vboxmanage', 'storagectl', 'UbuntuTest', '--name', 'SATA', '--add', 'sata'])
+            subprocess.call(['vboxmanage', 'storageattach', 'UbuntuTest', '--storagectl', 'SATA', '--port', '0', '--device',
+                             '0', '--type', 'hdd', '--medium', '/home/chevil/VirtualBox VMs/UbuntuTest/ubuntu1604.vdi'])
+
+            subprocess.call(['vboxmanage', 'storageattach', 'UbuntuTest', '--storagectl', 'SATA', '--port', '1', '--device',
+                             '0', '--type', 'dvddrive',
+                             '--medium', '/home/chevil/VirtualBox VMs/ubuntu-18.04.1-desktop-amd64.iso'])
+
+        subprocess.call(['vboxmanage', 'modifyvm', 'UbuntuTest', '--vrde', 'on'])
+        subprocess.call(['vboxmanage', 'modifyvm', 'UbuntuTest', '--vrdeaddress', '127.0.0.1'])
+        subprocess.call(['vboxmanage', 'modifyvm', 'UbuntuTest', '--vrdeport', '5901'])
+        subprocess.call(['vboxmanage', 'modifyvm', 'UbuntuTest', '--vrdeproperty', 'VNCPassword=12345678'])
+        subprocess.call(['vboxmanage', 'startvm', 'UbuntuTest'])
+
+        return 'http://127.0.0.1:8000'
+
+
--- index.py
+++ index.py
@@ -8,8 +8,19 @@ parser.add_argument('-a', '--action',
                     required=True
                     )
 
+parser.add_argument('-r', '--ram', type=int, help='RAM of a new machine')
+parser.add_argument('-hd', '--hdd', type=int, help='HDD of a new machine')
+parser.add_argument('-o', '--os', choices=['ubuntu'], type=str, help='Operation system of a new machine')
+
 args = vars(parser.parse_args())
+infrastructure_obj = InfrastructureHandler()
+
 
 if args.get('action') == 'init':
-    i = InfrastructureHandler()
-    i.init()
+    infrastructure_obj.init()
+
+
+if args.get('action') == 'install_vm':
+    # print(args.get('os'), args.get('ram'), args.get('hdd'))
+    vnc_ip = infrastructure_obj.install_vm(args.get('os'), args.get('ram'), args.get('hdd'))
+    print(vnc_ip)
