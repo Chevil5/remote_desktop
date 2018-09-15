a02371fb417e31faac06cb308f79774475624a62
Dmytro <dmytro@proisp.no>
2018-09-15 10:28:47 +0300

    Improvements of command line arguments

--- index.py
+++ index.py
@@ -2,7 +2,11 @@ import argparse
 from InfrastructureHandler import InfrastructureHandler
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-a', '--action', choices=['init', 'install', 'run'])
+parser.add_argument('-a', '--action',
+                    choices=['init', 'install_vm', 'run_vm'],
+                    help='Action it is basic argument witch set main goal of running script',
+                    required=True
+                    )
 
 args = vars(parser.parse_args())
 
