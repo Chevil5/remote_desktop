8477efb16dfbf092385172ada32c89365dd05abf
Dmytro <dmytro@proisp.no>
2018-09-15 10:22:31 +0300

    Permission fixes

--- InfrastructureHandler.py
+++ InfrastructureHandler.py
@@ -1,11 +1,8 @@
-import os
 import subprocess
 
 
 class InfrastructureHandler:
     def init(self):
-        password = '1111'
-
         vb_instalation_checking = subprocess.Popen(
             ["dpkg", "--get-selections", "virtualbox"],
             stdout=subprocess.PIPE,
@@ -15,12 +12,12 @@ class InfrastructureHandler:
         installed, noninstalled = vb_instalation_checking.communicate()
         if noninstalled:
             instalation_proc = subprocess.Popen(
-                ['sudo', '-S', 'apt-get', 'install', 'virtualbox', '-y'],
+                ['apt-get', 'install', 'virtualbox', '-y'],
                 stdin=subprocess.PIPE,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE)
-            print(instalation_proc.communicate(password+'\n'))
             out, er = instalation_proc.communicate()
+            instalation_proc
             print(out)
         else:
             print('VMBOX is installed')
